cd4d557531dd365995bdb7a30e49d3de053cf7e5
MbLespa <mbahlesky2@gmail.com>
2023-08-13 19:19:08 +0100

    schedules

--- src/app/transact/components/scheduleModal.jsx
+++ src/app/transact/components/scheduleModal.jsx
@@ -11,26 +11,48 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  CircularProgress,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AlertPopper, AutoCompleteInput } from "../../../components";
 import { mtnLogo, orangeLogo } from "../../../assets";
 
-const ScheduleModal = ({ isOpen, handleClose, title }) => {
-  const [accountNumber, setAccountNumber] = useState("");
-  const [receiversNumber, setReceiversNumber] = useState("");
-  const [amount, setAmount] = useState("");
-  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-  const [time, setTime] = useState(
-    new Date().toISOString().split("T")[1].slice(0, 5)
+const ScheduleModal = ({
+  isOpen,
+  handleClose,
+  title,
+  scheduleObject = null,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [schedule, setSchedule] = useState(null);
+
+  useEffect(() => {
+    if (scheduleObject) {
+      setSchedule(scheduleObject);
+    } else {
+      setSchedule({
+        accNumber: "",
+        recNumber: "",
+        amount: "",
+        date: new Date().toISOString().split("T")[0],
+        time: new Date().toISOString().split("T")[1].slice(0, 8),
+        frequency: "",
+        reminder: "",
+      });
+    }
+    setLoading(false);
+  }, [scheduleObject, title]);
+
+  const [canSelectDate, setCanSelectDate] = useState(
+    schedule && ["daily", "firstOfMonth", "lastOfMonth"].includes(schedule.date)
+      ? false
+      : true
   );
-  const [frequency, setFrequency] = useState("");
-  const [reminder, setReminder] = useState("Don't remind me");
-  const [canSelectDate, setCanSelectDate] = useState(true);
 
   const [accountNumberError, setAccountNumberError] = useState("");
   const [receiversNumberError, setReceiversNumberError] = useState("");
   const [dateError, setDateError] = useState("");
+  const [timeError, setTimeError] = useState("");
   const [amountError, setAmountError] = useState("");
   const [showAlert, setShowAlert] = useState(false);
 
@@ -46,37 +68,38 @@ const ScheduleModal = ({ isOpen, handleClose, title }) => {
       case "lastOfMonth":
         setCanSelectDate(false);
         break;
-      default: setCanSelectDate(true);
+      default:
+        setCanSelectDate(true);
     }
-    setFrequency(e.target.value);
-    console.log("frequency => ", frequency)
+    setSchedule({ ...schedule, frequency: e.target.value });
+    console.log("frequency => ", schedule.frequency);
   };
   const handleSubmit = (e) => {
     var isOk = true;
 
     if (isOk) {
-      if (accountNumber === "") {
+      if (schedule.accNumber === "") {
         setAccountNumberError("Please enter or select an account");
         isOk = false;
       } else {
         setAccountNumberError("");
       }
 
-      if (amount === "") {
+      if (schedule.amount === "") {
         setAmountError("Please enter amount");
         isOk = false;
       } else {
         setAmountError("");
       }
 
-      if (receiversNumber === "") {
+      if (schedule.recNumber === "") {
         setReceiversNumberError("Please enter receivers number");
         isOk = false;
       } else {
         setReceiversNumberError("");
       }
 
-      if (date === "") {
+      if (schedule.date === "") {
         setDateError("Please select receivers network");
         isOk = false;
       } else {
@@ -100,153 +123,191 @@ const ScheduleModal = ({ isOpen, handleClose, title }) => {
         className="fs-modal"
       >
         <Box variant="form" noValidate className="modal-body">
-          <Stack spacing={2}>
-            <Typography
-              color="primary"
-              variant="h6"
-              fontWeight="bold"
-              textAlign="center"
-            >
-              {title}
-            </Typography>
-            <Stack direction="row" flexWrap="wrap">
-              {accounts.map((account) => (
-                <Chip
-                  key={account.number}
-                  label={account.name}
-                  onClick={() => setAccountNumber(account.number)}
-                  color={
-                    accountNumber === account.number ? "primary" : "default"
-                  }
-                  variant={
-                    accountNumber === account.number ? "filled" : "outlined"
-                  }
-                  avatar={
-                    <Avatar
-                      alt={account.type}
-                      src={account.type === "mtn" ? mtnLogo : orangeLogo}
-                    />
-                  }
-                  clickable
-                />
-              ))}
-            </Stack>
-            <TextField
-              required
-              id="account-number"
-              label="Account Number"
-              variant="filled"
-              value={accountNumber}
-              onChange={(e) => setAccountNumber(e.target.value)}
-              placeholder="Enter account number"
-              error={accountNumberError !== ""}
-              helperText={accountNumberError}
-            />
-            <TextField
-              required
-              id="receivers-number"
-              label="Receivers Number"
-              variant="filled"
-              value={receiversNumber}
-              onChange={(e) => setReceiversNumber(e.target.value)}
-              placeholder="Enter receivers number"
-              error={receiversNumberError !== ""}
-              helperText={receiversNumberError}
-            />
-            <TextField
-              required
-              id="amount"
-              label="Amount"
-              variant="filled"
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="Enter Amount"
-              error={amountError !== ""}
-              helperText={amountError}
-            />
-
-            <FormControl error={dateError !== ""}>
-              <InputLabel id="frequency-label">Select Frequency</InputLabel>
-              <Select
-                labelId="frequency-label"
-                id="frequency"
-                label="How often..."
-                variant="filled"
-                value={frequency}
-                onChange={handleFrequencyChange}
-              >
-                <MenuItem value="firstOfMonth">
-                  First day of every month
-                </MenuItem>
-                <MenuItem value="lastOfMonth">Last day of every month</MenuItem>
-                <MenuItem value="monthly">Monthly</MenuItem>
-                <MenuItem value="weekly">Weekly</MenuItem>
-                <MenuItem value="daily">Daily</MenuItem>
-              </Select>
-            </FormControl>
-
-            <Stack direction="row" spacing={1}>
-              <TextField
-                required
-                id="date"
-                label="Date"
-                variant="filled"
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="Enter Amount"
-                error={dateError !== ""}
-                helperText={dateError}
-                sx={{ flexGrow: 1 }}
-                disabled={canSelectDate ? false : true}
-              />
-              <TextField
-                required
-                id="time"
-                label="Time"
-                variant="filled"
-                type="time"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-                placeholder="Time"
-                error={timeError !== ""}
-                helperText={timeError}
-                sx={{ flexGrow: 1 }}
-              />
-            </Stack>
-
-            <AutoCompleteInput 
-            options={["Don't remind me", '1 minute', '3 minutes', '5 minutes', '10 minutes', '30 minutes', '1 hours', '1 day']}
-            value={reminder}
-            onChange={setReminder}
-            placeholder='Set a reminder'
-            />
-
-            <Stack direction="row" spacing={2} justifyContent="space-between">
-              <Button
-                variant="contained"
-                color="error"
-                sx={{ flexGrow: 1 }}
-                onClick={() => handleClose()}
-              >
-                Cancel
-              </Button>
-              <Button
-                type="submit"
-                onClick={handleSubmit}
-                variant="contained"
+          {!loading ? (
+            <Stack spacing={2}>
+              <Typography
                 color="primary"
-                sx={{ flexGrow: 2 }}
+                variant="h6"
+                fontWeight="bold"
+                textAlign="center"
               >
-                Set
-              </Button>
+                {title}
+              </Typography>
+              <Stack direction="row" flexWrap="wrap">
+                {accounts.map((account) => (
+                  <Chip
+                    key={account.number}
+                    label={account.name}
+                    onClick={() =>
+                      setSchedule({ ...schedule, accNumber: account.number })
+                    }
+                    color={
+                      schedule.accNumber === account.number
+                        ? "primary"
+                        : "default"
+                    }
+                    variant={
+                      schedule.accNumber === account.number
+                        ? "filled"
+                        : "outlined"
+                    }
+                    avatar={
+                      <Avatar
+                        alt={account.type}
+                        src={account.type === "mtn" ? mtnLogo : orangeLogo}
+                      />
+                    }
+                    clickable
+                  />
+                ))}
+              </Stack>
+              <TextField
+                required
+                id="account-number"
+                label="Account Number"
+                variant="filled"
+                value={schedule.accNumber}
+                onChange={(e) =>
+                  setSchedule({ ...schedule, accNumber: e.target.value })
+                }
+                placeholder="Enter account number"
+                error={accountNumberError !== ""}
+                helperText={accountNumberError}
+              />
+              <TextField
+                required
+                id="receivers-number"
+                label="Receivers Number"
+                variant="filled"
+                value={schedule.recNumber}
+                onChange={(e) =>
+                  setSchedule({ ...schedule, recNumber: e.target.value })
+                }
+                placeholder="Enter receivers number"
+                error={receiversNumberError !== ""}
+                helperText={receiversNumberError}
+              />
+              <TextField
+                required
+                id="amount"
+                label="Amount"
+                variant="filled"
+                type="number"
+                value={schedule.amount}
+                onChange={(e) =>
+                  setSchedule({ ...schedule, amount: e.target.value })
+                }
+                placeholder="Enter Amount"
+                error={amountError !== ""}
+                helperText={amountError}
+              />
+
+              <FormControl error={dateError !== ""}>
+                <InputLabel id="frequency-label">Select Frequency</InputLabel>
+                <Select
+                  labelId="frequency-label"
+                  id="frequency"
+                  label="How often..."
+                  variant="filled"
+                  value={schedule.frequency}
+                  onChange={handleFrequencyChange}
+                >
+                  <MenuItem value="firstOfMonth">
+                    First day of every month
+                  </MenuItem>
+                  <MenuItem value="lastOfMonth">
+                    Last day of every month
+                  </MenuItem>
+                  <MenuItem value="monthly">Monthly</MenuItem>
+                  <MenuItem value="weekly">Weekly</MenuItem>
+                  <MenuItem value="daily">Daily</MenuItem>
+                </Select>
+              </FormControl>
+
+              <Stack direction="row" spacing={1}>
+                <TextField
+                  required
+                  id="date"
+                  label="Date"
+                  variant="filled"
+                  type="date"
+                  value={schedule.date}
+                  onChange={(e) =>
+                    setSchedule({ ...schedule, date: e.target.value })
+                  }
+                  placeholder="Enter Amount"
+                  error={dateError !== ""}
+                  helperText={dateError}
+                  sx={{ flexGrow: 1 }}
+                  disabled={canSelectDate ? false : true}
+                />
+                <TextField
+                  required
+                  id="time"
+                  label="Time"
+                  variant="filled"
+                  type="time"
+                  value={schedule.time}
+                  onChange={(e) =>
+                    setSchedule({ ...schedule, time: e.target.value })
+                  }
+                  placeholder="Time"
+                  error={timeError !== ""}
+                  helperText={timeError}
+                  sx={{ flexGrow: 1 }}
+                />
+              </Stack>
+
+              <AutoCompleteInput
+                options={[
+                  "Don't remind me",
+                  "1 minute",
+                  "3 minutes",
+                  "5 minutes",
+                  "10 minutes",
+                  "30 minutes",
+                  "1 hours",
+                  "1 day",
+                ]}
+                value={schedule.reminder}
+                onChange={(val) => setSchedule({ ...schedule, reminder: val })}
+                variant="filled"
+                placeholder="Set a reminder"
+              />
+
+              <Stack direction="row" spacing={2} justifyContent="space-between">
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ flexGrow: 1 }}
+                  onClick={() => handleClose()}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  onClick={handleSubmit}
+                  variant="contained"
+                  color="primary"
+                  sx={{ flexGrow: 2 }}
+                >
+                  Set
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
+          ) : (
+            <CircularProgress />
+          )}
         </Box>
       </Modal>
 
-      <AlertPopper alertType='success' showAlert={showAlert} handleClose={() => setShowAlert(false)}>Schedule Set</AlertPopper>
+      <AlertPopper
+        alertType="success"
+        showAlert={showAlert}
+        handleClose={() => setShowAlert(false)}
+      >
+        Schedule Set
+      </AlertPopper>
     </>
   );
 };
--- src/app/transact/pages/schedule.jsx
+++ src/app/transact/pages/schedule.jsx
@@ -1,14 +1,208 @@
+import { Add, Close, Delete, Edit, TitleRounded } from "@mui/icons-material";
+import {
+  Card,
+  Grid,
+  Table,
+  TableBody,
+  TableContainer,
+  TableRow,
+  TableCell,
+  Button,
+  Fab,
+  Avatar,
+  Stack
+} from "@mui/material";
+import { useState } from "react";
+import ScheduleModal from "../components/scheduleModal";
+import { mtnLogo, orangeLogo } from "../../../assets";
+
+const style = {
+  fontWeight: "bold",
+  fontSize: {
+    xs: 14,
+    md: 16,
+  },
+};
+
+const normalStyles = {
+  fontSize: {
+    xs: 12,
+    md: 14,
+  },
+};
+
+const accounts = [
+  { name: "Main Account", number: "237679682626", type: "mtn" },
+  { name: "Second Account", number: "237672074638", type: "orange" },
+];
+
 const SchedulePage = () => {
-    return ( 
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: 'calc(100vh - 50px)'
-        }}>
-            <h2>Schedule</h2>
-        </div>
-     );
-}
- 
+  const [title, setTitle] = useState("Edit Schedule");
+  const [openScheduleModal, setOpenScheduleModal] = useState(false);
+  const [editSchedule, setEditSchedule] = useState(null);
+  const [schedules, setSchedules] = useState([
+    {
+      accNumber: "237679682626",
+      recNumber: "689928398",
+      amount: 10000,
+      date: new Date("2023-03-05").toISOString().split("T")[0],
+      time: new Date("2023-03-05T09:00:00Z")
+        .toISOString()
+        .split("T")[1]
+        .slice(0, 5),
+      frequency: "monthly",
+      reminder: "3 minutes",
+    },
+    {
+      accNumber: "237672074638",
+      recNumber: "689928398",
+      amount: 5000,
+      date: new Date("2023-05-08").toISOString().split("T")[0],
+      time: new Date("2023-03-05T11:00:00Z")
+        .toISOString()
+        .split("T")[1]
+        .slice(0, 5),
+      frequency: "weekly",
+      reminder: "1 minute",
+    },
+    {
+      accNumber: "679682426",
+      recNumber: "689928398",
+      amount: 15000,
+      date: new Date("2023-03-05").toISOString().split("T")[0],
+      time: new Date("2023-03-05T05:00:00Z")
+        .toISOString()
+        .split("T")[1]
+        .slice(0, 8),
+      frequency: "lastOfMonth",
+      reminder: "3 minutes",
+    },
+  ]);
+
+  const handleEdit = (schedule, title) => {
+    setTitle(title);
+    setEditSchedule(schedule);
+    setOpenScheduleModal(true);
+  };
+
+  return (
+    <div>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent={{ xs: "start", sm: "center", md: "start" }}
+        padding={2}
+      >
+        {schedules.map((schedule) => (
+          <Grid item xs={12} sm={9} md={6}>
+            <TableContainer component={Card} backgroundColor="primary">
+              <Table
+                size="small"
+                sx={{ backgroundColor: (theme) => theme.palette.primary.light }}
+              >
+                <TableBody>
+                  <TableRow>
+                    <TableCell sx={style}>Account Number</TableCell>
+                    <TableCell sx={normalStyles}>
+                      {(() => {
+                        const acc = accounts.find(
+                          (account) => account.number === schedule.accNumber
+                        );
+                        if (acc) {
+                          return (
+                            <Stack direction='row' alignItems='center'>
+                              <Avatar
+                                src={acc.type === "mtn" ? mtnLogo : orangeLogo}
+                                alt={acc.type}
+                                sx={{width: '20px', height: '20px'}}
+                              />
+                              {acc.name} ({schedule.accNumber})
+                            </Stack>
+                          );
+                        } else {
+                          return schedule.accNumber;
+                        }
+                      })()}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={style}>Receiver's Number</TableCell>
+                    <TableCell sx={normalStyles}>
+                      {schedule.recNumber}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={style}>Amount</TableCell>
+                    <TableCell sx={normalStyles}>
+                      {schedule.amount} FCFA
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={style}>Date & time</TableCell>
+                    <TableCell sx={normalStyles}>
+                      {schedule.date} - {schedule.time}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={style}>Frequency</TableCell>
+                    <TableCell sx={normalStyles}>
+                      {schedule.frequency}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell sx={style}>Reminder</TableCell>
+                    <TableCell sx={normalStyles}>{schedule.reminder}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <Button
+                        type="button"
+                        variant="contained"
+                        size="small"
+                        startIcon={<Edit />}
+                        onClick={() => handleEdit(schedule, "Edit Schedule")}
+                      >
+                        Edit
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        type="button"
+                        variant="contained"
+                        size="small"
+                        color="error"
+                        startIcon={<Delete />}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        ))}
+      </Grid>
+
+      <div style={{ position: "absolute", bottom: "50px", right: "50px" }}>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={() => handleEdit(null, "New Schedule")}
+        >
+          <Add />
+        </Fab>
+      </div>
+
+      <ScheduleModal
+        isOpen={openScheduleModal}
+        handleClose={() => setOpenScheduleModal(false)}
+        title={title}
+        scheduleObject={editSchedule}
+      />
+    </div>
+  );
+};
+
 export default SchedulePage;
